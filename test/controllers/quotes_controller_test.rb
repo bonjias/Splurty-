23e3978bdab20f87e1d93ad2671865a4d29fc84c
Jonathan Bias <bonjias@gmail.com>
2015-09-15 21:20:24 +0000

    controller tests done

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "quote show page" do
+     quote = Quote.create(:author => 'Bon Jias', :saying => 'TDD all day')
+   get :show, :id => quote.id 
+   assert_response :success 
+   end
+
+
+   test "quote show page, not found " do 
+   	get :show, :id => 'omg'
+	assert_response :not_found 
+   end 
+
+
+
 end
